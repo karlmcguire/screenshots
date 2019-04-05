aa27adf71549b3400ee4f9ae959ce48672470e9d
Karl McGuire <karl@xba.io>
2019-04-05 19:49:36 -0400

    more posts

--- docs/js/model.js
+++ docs/js/model.js
@@ -20,6 +20,36 @@ let model = {
             user: "0x3639",
             date: new Date("November 19, 2018 00:00:00"), 
             image: "https://i.imgur.com/tijPYBg.png"
+        },
+        {
+            title: "back into the fold",
+            user: "WIldefyr",
+            date: new Date("March 3, 2017 00:00:00"), 
+            image: "https://i.redd.it/caq37bqomkn21.png"
+        },
+        {
+            title: "throwback to my bean days",
+            user: "jmbi",
+            date: new Date("February 13, 2019 00:00:00"), 
+            image: "https://i.imgur.com/BziH1iE.png"
+        },
+        {
+            title: "Running Fluxbox at work.",
+            user: "xexpanderx",
+            date: new Date("April 1, 2019 00:00:00"), 
+            image: "https://i.redd.it/6053z0kg80i01.png"
+        },
+        {
+            title: "start tinkering now",
+            user: "dcat",
+            date: new Date("April 15, 2015 00:00:00"), 
+            image: "http://pub.iotek.org/p/s4joyQK.png"
+        },
+        {
+            title: "transparent double borders?",
+            user: "dcat",
+            date: new Date("March 13, 2016 00:00:00"), 
+            image: "https://pub.iotek.org/p/r5Abt5U.png"
         }
     ]
 };
