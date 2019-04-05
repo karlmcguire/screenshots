96950bc023ee1211b2df0614f74639eba05df722
Karl McGuire <karl@xba.io>
2019-04-05 17:48:39 -0400

    jquery stuff

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,18 +4,30 @@ import { model } from "./model.js";
 import { view } from "./view.js";
 import { action } from "./action.js";
 
+const render = (path) => {
+    $("header").html(view.nav.render(path));
+
+    $("main").html(view.main.render(path));
+    view.nap[path]();
+};
+
 $(() => {
-    $("header").html(view.nav.render(window.location.pathname));   
+    render(window.location.pathname);
 
     $("header").on("click", ".nav__link", function(e) {
         e.preventDefault();
-    
+   
+        // href of the clicked link
         let href = $(this).attr("href");
+
+        // if the page is already active, don't do anything
+        if (href == window.location.pathname) {
+            return; 
+        }
 
         // change the url
         window.history.pushState({}, href, window.location.origin + href);
         
-        // render the nav with the new active link 
-        $("header").html(view.nav.render(href));
+        render(href); 
     });
 });
--- docs/js/view.js
+++ docs/js/view.js
@@ -13,19 +13,44 @@ const links = {
 
 const view = {
     nav: {},
+    main: {},
+    page: {},
+    nap: {}
 };
 
-view.nav.link = (text, href, active) => 
-    `<a href="` + href + `" class="nav__link` + 
-        ((active) ? ` nav__link--active` : ``) + `">` + text + `</a>`;
+///////////////////////////////////////////////////////////////////////////////
 
-view.nav.render = (active) =>
-    `<nav><div class="nav__links nav__links--left">` + 
+view.nav.link = (text, href, path) => 
+    `<a href="` + href + `" class="nav__link` + 
+        ((path) ? ` nav__link--active` : ``) + `">` + text + `</a>`;
+
+view.nav.render = (path) =>
+    `<div class="wrap"><nav><div class="nav__links nav__links--left">` + 
         links.left.reduce((html, link) => html +
-            view.nav.link(link.text, link.href, link.href == active), "") +
+            view.nav.link(link.text, link.href, link.href == path), "") +
     `</div>` + `<div class="nav__links nav__links--right">` +
         links.right.reduce((html, link) => html +
-            view.nav.link(link.text, link.href, link.href == active), "") +
-    `<div></nav>`;
+            view.nav.link(link.text, link.href, link.href == path), "") +
+    `</div></nav></div>`;
+
+view.main.render = (path) =>
+    `<div class="wrap"><div class="content">` + 
+        view.page[path]() + `</div></div>`;
+
+///////////////////////////////////////////////////////////////////////////////
+
+view.page["/"]        = () => `welcome home!`;
+view.page["/about"]   = () => "this is the about page!";
+view.page["/sign_up"] = () => "sign up here!";
+view.page["/sign_in"] = () => "sign in here!";
+
+///////////////////////////////////////////////////////////////////////////////
+
+view.nap["/"]        = () => {};
+view.nap["/about"]   = () => {};
+view.nap["/sign_up"] = () => {};
+view.nap["/sign_in"] = () => {};
+
+///////////////////////////////////////////////////////////////////////////////
 
 export { view };
